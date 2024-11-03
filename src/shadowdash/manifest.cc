078dca392e293038ed94cda0a8577e64a3a18116
cchels <114442802+cchels@users.noreply.github.com>
2024-11-02 22:10:38 -0400

    Update manifest.cc: using binding for pool depth, add default statement and phony

--- src/shadowdash/manifest.cc
+++ src/shadowdash/manifest.cc
@@ -5,37 +5,46 @@ using namespace shadowdash;
 void manifest() {
   let(flags, "-O3");
 
-  auto heavy_object_pool = 4;
+  let(pool_depth, "4");
 
-  auto compile = rule{ {
+  pool_ heavy_object_pool(bind(depth, "pool_depth"_v));
+
+  auto compile = rule( {
       bind(command, "g++", "flags"_v, "-c", in, "-o", out),  
-      bind(pool, "heavy_object_pool"_p)
-  } };
+      bind(pool, "heavy_object_pool"_v)
+  } );
 
-  auto link = rule{ {
-      bind(command, "g++", in, "-o", out),  //
-  } };
+  auto link = rule( {
+      bind(command, "g++", in, "-o", out), 
+  } );
 
-  build(list{ str{ "hello.o" } },   //
-        {},                         //
-        compile,                    //
-        list{ str{ "hello.cc" } },  //
-        {},                         //
-        {},                         //
-        { 
-          bind(flags, "-O2"),
-          bind(pool, ""_p)
-        }      //
+  auto build_c = build(list{ str{ "hello.o" } }, 
+        {},
+        compile,
+        list{ str{ "hello.cc" } },
+        {},
+        {},
+        { bind(flags, "-O2") }
   );
 
-  build(list{ str{ "hello" } },    //
-        {},                        //
-        link,                      //
-        list{ str{ "hello.o" } },  //
-        {},                        //
-        {},                        //
-        {}                         //
+  auto build_l= build(list{ str{ "hello" } },
+        {},
+        link,
+        list{ str{ "hello.o" } },
+        {},
+        {},
+        {}
+  );
+
+  auto build_p= build(list{ str{ "dummy" } },
+        {},
+        phony,
+        {},
+        {},
+        {},
+        {}
   );
 
   default_(list{ str{ "hello" } });
+  default_(list{ str{ "foo1" }, str{"foo2"} });
 }
