161a1fdcc0e66e760ab4a109cad7f7171e36b4e7
qianxichen233 <qc815@nyu.edu>
2024-11-20 23:14:45 +0000

    work on manifest.h

--- shadowdash_test/manifest.cc
+++ shadowdash_test/manifest.cc
@@ -3,33 +3,23 @@
 
 using namespace shadowdash;
 
-Token operator"" _l(const char* value, std::size_t len) {
-    return Token(Token::Type::LITERAL, std::string(value, len));
-}
-
-Token operator"" _v(const char* value, std::size_t len) {
-    return Token(Token::Type::VAR, std::string(value, len));
-}
-
-rule phony = rule::phony;
-
-buildGroup manifest() {
+void manifest() {
 	std::cout << "manifest start" << std::endl;
-	static auto compile = rule{"compile", {
+	rule{"compile", {
 		bind(command, {"g++", " ", "-c", " ", "in"_v, " ", "-o", " ", "out"_v}),
 		bind(depfile, {"out"_v, ".d"}),
 		bind(deps, {"gcc"})
 	}};
 	std::cout << "compile rule done" << std::endl;
 
-	static auto link = rule{"link", {
+	rule{"link", {
 		bind(command, {"g++", " ", "in"_v, " ", "-o", " ", "out"_v}),
 	}};
 	std::cout << "link rule done" << std::endl;
 
-	static auto build1 = build(list{ {str {{"hello.o"}}} },
+	build(list{ {str {{"hello.o"}}} },
 		list{{}},
-		compile,
+		"compile",
 		list{ {str{{"hello.cc"}}} },
 		list{{}},
 		list{{}},
@@ -37,9 +27,9 @@ buildGroup manifest() {
 	);
 	std::cout << "build1 done" << std::endl;
 
-	static auto build2 = build(list{ {str {{"hello"}}} },
+	build(list{ {str {{"hello"}}} },
 		list{{}},
-		link,
+		"link",
 		list{ {str{{"hello.o"}}} },
 		list{{}},
 		list{{}},
@@ -47,9 +37,9 @@ buildGroup manifest() {
 	);
 	std::cout << "build2 done" << std::endl;
 
-	static auto build3 = build(list{ {str {{"hello.d"}}} },
+	build(list{ {str {{"hello.d"}}} },
 		list{{}},
-		phony,
+		"phony",
 		list{{}},
 		list{{}},
 		list{{}},
@@ -57,18 +47,5 @@ buildGroup manifest() {
 	);
 	std::cout << "build3 done" << std::endl;
 
-	// std::cout << "print build1" << std::endl;
-	// std::cout << build1 << std::endl;
-	// std::cout << "done printing build1" << std::endl;
-
-	// std::cout << "print build2" << std::endl;
-	// std::cout << build2 << std::endl;
-	// std::cout << "done printing build2" << std::endl;
-
-	// std::cout << "print build3" << std::endl;
-	// std::cout << build3 << std::endl;
-	// std::cout << "done printing build3" << std::endl;
-
 	std::cout << "manifest end" << std::endl;
-	return buildGroup({ build1, build2, build3 });
 }
--- src/shadowdash_manifest.h
+++ src/shadowdash_manifest.h
@@ -1,4 +1,4 @@
-#include "ninja.h"
+#include "shadowdash_include/state.h"
 #pragma once
 
 #include <iostream>
@@ -15,88 +15,32 @@ public:
     enum Type { LITERAL, VAR };
 
     Token(Type type, std::string value)
-        : type_(type), value_(value) {
-        //std::cout << "creating " << type << " : " << value_ << std::endl;
-    }
+        : type_(type), value_(std::move(value)) {}
 
-    Token(std::string value) : Token(Token::LITERAL, value) {
-        //std::cout << "creating LITERAL: " << value_ << std::endl;
-    }
+    Token(std::string value) : Token(Token::LITERAL, std::move(value)) {}
 
-    Token(const char* value) : Token(Token::LITERAL, std::string(value)) {
-        // std::cout << "creating LITERAL: " << value << std::endl;
-    }
-
-    ~Token() {
-        // std:: cout << this->value_ << " destroyed" << std::endl;
-    }
+    Token(const char* value) : Token(Token::LITERAL, std::string(value)) {}
 
     Type type_;
     std::string value_;
 };
 
-// Overload operator<< for Token
-// std::ostream& operator<<(std::ostream& os, const Token& token) {
-//     os << (token.type_ == Token::LITERAL ? "LITERAL" : "VAR") << ": " << token.value_;
-//     return os;
-// }
-
-// Token operator"" _l(const char* value, std::size_t len) {
-//     return Token(Token::Type::LITERAL, std::string(value, len));
-// }
-
-// Token operator"" _v(const char* value, std::size_t len) {
-//     return Token(Token::Type::VAR, std::string(value, len));
-// }
-
 class str {
 public:
-    str(std::vector<Token> tokens) : tokens_(tokens) {
-        //std::cout << "creating str: ";
-        // for (const auto& token : tokens_) {
-            //std::cout << token << " ";
-        // }
-        //std::cout << std::endl;
-    }
+    str(std::vector<Token> tokens) : tokens_(std::move(tokens)) {}
 
     std::vector<Token> tokens_;
 };
-
-// Overload operator<< for str
-// std::ostream& operator<<(std::ostream& os, const str& s) {
-//     os << "str: [";
-//     for (const auto& token : s.tokens_) {
-//         os << token << ", ";
-//     }
-//     os << "]";
-//     return os;
-// }
 
 using binding = std::pair<std::string, str>;
 using map = std::vector<binding>;
 
 class list {
 public:
-    list(std::vector<str> values) : values_(values) {
-        //std::cout << "creating list: ";
-        // for (const auto& value : values_) {
-        //     std::cout << value << " ";
-        // }
-        //std::cout << std::endl;
-    }
+    list(std::vector<str> values) : values_(std::move(values)) {}
 
     std::vector<str> values_;
 };
-
-// Overload operator<< for list
-// std::ostream& operator<<(std::ostream& os, const list& l) {
-//     os << "list: [";
-//     for (const auto& value : l.values_) {
-//         os << value << ", ";
-//     }
-//     os << "]";
-//     return os;
-// }
 
 class var {
 public:
@@ -106,230 +50,106 @@ public:
     str value_;
 };
 
-// Overload operator<< for var
-// std::ostream& operator<<(std::ostream& os, const var& v) {
-//     os << "var: " << v.name_ << ", " << v.value_;
-//     return os;
-// }
-
-
-// class rule {
-// public:
-//     enum SPECIAL_RULE {
-//         phony
-//     };
-
-//     union _rule {
-//         _rule() : sp_rule(phony) {}
-//         _rule(map bindings) : bindings_(bindings) {}
-//         _rule(SPECIAL_RULE sr) : sp_rule(sr) {}
-//         ~_rule() {}
-
-//         map bindings_;
-//         SPECIAL_RULE sp_rule;
-//     } _rule_data;
-//     bool is_special;
-
-//     rule(map bindings) : _rule_data(bindings), is_special(false) {}
-//     rule(SPECIAL_RULE sp_rule) : _rule_data(sp_rule), is_special(true) {}
-
-//     rule(const rule& other) : is_special(other.is_special) {
-//         if (is_special) {
-//             new (&_rule_data) _rule(other._rule_data.sp_rule);  // Copy the special rule
-//         } else {
-//             new (&_rule_data) _rule(other._rule_data.bindings_);  // Copy the bindings map
-//         }
-//     }
-// };
-
 class rule {
 public:
-    enum RULE_TYPE {
-        normal,
-        phony
-    };
-
-    std::string name;
-    RULE_TYPE type;
-    map bindings_;
-
-    rule(const char* name, map bindings) : name(std::string(name)), bindings_(bindings), type(normal) {
-        std::cout << "ninja state address: " << &ninja_state << std::endl;
+    rule(const char* name, map bindings) {
+        if(name == "phony") return;
         Rule* rule_ninja = new Rule(std::string(name));
-        for (const auto& binding : this->bindings_) {
+        for (const auto& binding : bindings) {
             EvalString *value = new EvalString;
             for(shadowdash::Token token : binding.second.tokens_)
             {
                 if(token.type_ == token.LITERAL)
-                    value->AddText(token.value_);
+                    value->AddText(std::move(token.value_));
                 else if(token.type_ == token.VAR)
-                    value->AddSpecial(token.value_);
+                    value->AddSpecial(std::move(token.value_));
             }
-            rule_ninja->AddBinding(std::string(binding.first), *value);
+            rule_ninja->AddBinding(std::move(binding.first), *value);
         }
         ninja_state->bindings_.AddRule(rule_ninja);
     }
-    rule(RULE_TYPE type) : name("phony"), bindings_({}), type(phony) {
-        // do nothing since ninja_state has phony rule by default
-    }
 };
-
-// Overload operator<< for rule
-// std::ostream& operator<<(std::ostream& os, const rule& r) {
-//     os << "rule: [";
-//     if(r.type == rule::normal)
-//         os << r.type;
-//     else {
-//         for (const auto& binding : r.bindings_) {
-//             os << binding.first << ": " << binding.second << ", ";
-//         }
-//     }
-//     os << "]";
-//     return os;
-// }
 
 class build {
 public:
     build(
         list outputs,
         list implicit_outputs,
-        rule &rule,
+        const char* rule,
         list inputs,
         list implicit_inputs,
         list order_only_inputs,
-        map bindings)
-        : outputs_(outputs),
-          implicit_outputs_(implicit_outputs),
-          rule_(rule),
-          inputs_(inputs),
-          implicit_inputs_(implicit_inputs),
-          order_only_inputs_(order_only_inputs),
-          bindings_(bindings) {
-            const Rule* rule_ninja = ninja_state->bindings_.LookupRule(std::string(this->rule_.name));
+        map bindings){
+            const Rule* rule_ninja = ninja_state->bindings_.LookupRule(rule);
             if(!rule_ninja) return;
             Edge* edge = ninja_state->AddEdge(rule_ninja);
-            string *err = new string();
+            std::string *err = new std::string();
             
-            for(const shadowdash::str& str : this->outputs_.values_) {
-                string output;
+            for(const shadowdash::str& str : outputs.values_) {
+                std::string output;
                 for (const shadowdash::Token& token: str.tokens_) {
-                    output.append(token.value_);
+                    output.append(std::move(token.value_));
                 }
-                ninja_state->AddOut(edge, output, 0, err);
+                ninja_state->AddOut(edge, std::move(output), 0, err);
             }
 
             int implicit_output = 0;
-            for(const shadowdash::str& str : this->implicit_outputs_.values_) {
-                string output;
+            for(const shadowdash::str& str : implicit_outputs.values_) {
+                std::string output;
                 for (const shadowdash::Token& token: str.tokens_) {
-                    output.append(token.value_);
+                    output.append(std::move(token.value_));
                 }
                 ++implicit_output;
-                ninja_state->AddOut(edge, output, 0, err);
+                ninja_state->AddOut(edge, std::move(output), 0, err);
             }
             edge->implicit_outs_ = implicit_output;
 
-            for(const shadowdash::str& str : this->inputs_.values_) {
-                string input;
+            for(const shadowdash::str& str : inputs.values_) {
+                std::string input;
                 for (const shadowdash::Token& token: str.tokens_) {
-                    input.append(token.value_);
+                    input.append(std::move(token.value_));
                 }
-                ninja_state->AddIn(edge, input, 0);
+                ninja_state->AddIn(edge, std::move(input), 0);
             }
 
             int implicit_input = 0;
-            for(const shadowdash::str& str : this->implicit_inputs_.values_) {
-                string input;
+            for(const shadowdash::str& str : implicit_inputs.values_) {
+                std::string input;
                 for (const shadowdash::Token& token: str.tokens_) {
-                    input.append(token.value_);
+                    input.append(std::move(token.value_));
                 }
                 ++implicit_input;
-                ninja_state->AddIn(edge, input, 0);
+                ninja_state->AddIn(edge, std::move(input), 0);
             }
 
             int order_only = 0;
-            for(const shadowdash::str& str : this->order_only_inputs_.values_) {
-                string input;
+            for(const shadowdash::str& str : order_only_inputs.values_) {
+                std::string input;
                 for (const shadowdash::Token& token: str.tokens_) {
-                    input.append(token.value_);
+                    input.append(std::move(token.value_));
                 }
                 ++order_only;
-                ninja_state->AddIn(edge, input, 0);
+                ninja_state->AddIn(edge, std::move(input), 0);
             }
             edge->implicit_deps_ = implicit_input;
             edge->order_only_deps_ = order_only;
 
             BindingEnv *env_ = &ninja_state->bindings_;
             BindingEnv* env = new BindingEnv(env_);
-            for (const auto& binding : this->bindings_) {
+            for (const auto& binding : bindings) {
                 EvalString *value = new EvalString;
-                bool first_iteration = true;
                 for(shadowdash::Token token : binding.second.tokens_)
                 {
                     if(token.type_ == token.LITERAL)
-                        value->AddText(token.value_);
+                        value->AddText(std::move(token.value_));
                     else if(token.type_ == token.VAR)
-                        value->AddSpecial(token.value_);
-                    first_iteration = false;
+                        value->AddSpecial(std::move(token.value_));
                 }
-                env->AddBinding(std::string(binding.first), value->Evaluate(env_));
+                env->AddBinding(std::move(binding.first), value->Evaluate(env_));
             }
             edge->env_ = env;
     }
-
-    // build(const build& other)
-    //     : outputs_(other.outputs_),
-    //       implicit_outputs_(other.implicit_outputs_),
-    //     //   rule_(other.rule_),
-    //       inputs_(other.inputs_),
-    //       implicit_inputs_(other.implicit_inputs_),
-    //       order_only_inputs_(other.order_only_inputs_),
-    //       bindings_(other.bindings_) {
-    //         this->rule_ = other.rule_;
-    //       }
-
-    list outputs_;
-    list implicit_outputs_;
-    rule &rule_;
-    list inputs_;
-    list implicit_inputs_;
-    list order_only_inputs_;
-    map bindings_;
 };
-
-// Overload operator<< for build
-// std::ostream& operator<<(std::ostream& os, const build& b) {
-//     os << "build: { "
-//        << "outputs: " << b.outputs_ << ", "
-//        << "implicit_outputs: " << b.implicit_outputs_ << ", "
-//        << "rule: " << b.rule_ << ", "
-//        << "inputs: " << b.inputs_ << ", "
-//        << "implicit_inputs: " << b.implicit_inputs_ << ", "
-//        << "order_only_inputs: " << b.order_only_inputs_ << ", "
-//        << "bindings: [";
-//     for (const auto& binding : b.bindings_) {
-//         os << binding.first << ": " << binding.second << ", ";
-//     }
-//     os << "] }";
-//     return os;
-// }
-
-class buildGroup {
-public:
-    buildGroup(std::vector<build> builds) : builds(builds) {}
-
-    std::vector<build> builds;
-};
-
-// Overload operator<< for buildGroup
-// std::ostream& operator<<(std::ostream& os, const buildGroup& bg) {
-//     os << "buildGroup: [";
-//     for (const auto& b : bg.builds) {
-//         os << b << ", ";
-//     }
-//     os << "]";
-//     return os;
-// }
 
 Token operator"" _l(const char* value, std::size_t len) {
     return Token(Token::Type::LITERAL, std::string(value, len));
@@ -338,8 +158,6 @@ Token operator"" _l(const char* value, std::size_t len) {
 Token operator"" _v(const char* value, std::size_t len) {
     return Token(Token::Type::VAR, std::string(value, len));
 }
-
-static rule phony = rule::phony;
 
 static auto in = "in"_v;
 static auto out = "out"_v;
